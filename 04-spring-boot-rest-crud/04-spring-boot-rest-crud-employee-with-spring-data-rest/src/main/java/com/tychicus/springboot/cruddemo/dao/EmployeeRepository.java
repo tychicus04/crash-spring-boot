cee90ae69e10d16f8761b28e4073794e34bc6e69
Nguyen Minh Tai <nmt.1x10x100@gmail.com>
2024-02-14 23:12:57 +0700

    config and sorting

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/tychicus/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/tychicus/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.tychicus.springboot.cruddemo.dao;
 
 import com.tychicus.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 //    that's it ... no need to write anything LOL!

01d18b5062a717d0591816b0e7dd0df679513656
Nguyen Minh Tai <nmt.1x10x100@gmail.com>
2024-02-16 00:53:52 +0700

    update BCrypt Encryption, JDBC Authentication and custom table

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/tychicus/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/tychicus/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -8,10 +8,50 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+//    add support for JDBC ... no more hardcoded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+//        define to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+
+//        define to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+             .authorizeHttpRequests(
+                     configurer -> configurer
+                             .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                             .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                             .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                             .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                             .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+             );
+
+//        use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+//        disable cross site request forgery (CSRF protection)
+//        in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
@@ -35,25 +75,5 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-             .authorizeHttpRequests(
-                     configurer -> configurer
-                             .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                             .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                             .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                             .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                             .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-             );
-
-//        use HTTP basic authentication
-        http.httpBasic(Customizer.withDefaults());
-
-//        disable cross site request forgery (CSRF protection)
-//        in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
-        http.csrf(csrf -> csrf.disable());
-
-        return http.build();
-    }
+     */
 }

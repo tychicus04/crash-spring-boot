634eca9859bc6a2374ba2d8f247a5cb43f5bf88f
Nguyen Minh Tai <nmt.1x10x100@gmail.com>
2024-02-15 23:50:39 +0700

    update

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/tychicus/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.tychicus.springboot.cruddemo.service;
+
+import com.tychicus.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
